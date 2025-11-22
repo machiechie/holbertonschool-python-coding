5b5b7ff692cba8ca08662229760ba3d84e968cca
machiechie <heyderli_005@mail.ru>
2025-11-22 12:59:26 +0000

    sdff

---
+++ python-classes/4-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+Defines a Square class with private size attribute and input validation.
+"""
+
+
+class Square:
+    """
+    Represents a square with size validation upon instantiation.
+
+    Attributes:
+        __size (int): The size of the square (private).
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the new square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size = 0:
+            print()
+        else:
+            print("#{}".format(self.__size))
