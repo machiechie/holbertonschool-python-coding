8f0ae64286534f6583ef31e9baa809ae28c17939
machiechie <heyderli_005@mail.ru>
2025-11-22 12:53:43 +0000

    sdff

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -29,9 +29,11 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
     def size(self):
         return self.__size
 
+    @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
