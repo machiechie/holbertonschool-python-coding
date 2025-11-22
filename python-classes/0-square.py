e9d69163e4ef2ccd39103f0026b92a663328b67b
machiechie <heyderli_005@mail.ru>
2025-11-22 11:56:01 +0000

    adgag

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 class Square:
     def _init_(self,size):
-        Args:
-            _size(int) = The size of the squiare
+        _size(int) = The size of the squiare
         self._size = size
