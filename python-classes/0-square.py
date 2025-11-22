dd44ec5a3d8409dc2e66c0479f825741c00c7d56
machiechie <heyderli_005@mail.ru>
2025-11-22 12:22:55 +0000

    sdff

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+"""
+Defines a Square class.
+"""
+
 class Square:
-def init(self, size):
-self.__size = size
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square (private attribute).
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
