75aad95e2f85eb8c652abeb4611c36cd763e5f7f
machiechie <heyderli_005@mail.ru>
2025-11-22 12:02:03 +0000

    adgag

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-
-"""Define a class Square."""
-
+"""0-square module
+Defines a class Square with a private size attribute.
+"""
 
 class Square:
-    """Empty class that defines a square"""
-    pass
+"""Represents a square with a private size attribute."""
+
+def __init__(self, size):
+    """Initialize a new Square instance.
+
+    Args:
+        size (int): The size of the square.
+    """
+    self.__size = size
